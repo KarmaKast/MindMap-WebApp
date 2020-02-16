88a617d6e5ef01163105b874fccdda0f79c2dbf4
Sree Chandan <sreechandan98@gmail.com>
2020-02-16 08:07:34 +0530

    installed electron builder plugin

---
+++ src/components/general/PopupWindow.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+<script>
+/*
+a wrapper component that handles z-index elevation,
+
+*/
+export default {
+  name: "PopupWindow"
+};
+</script>
+<style lang="sass" scoped></style>
