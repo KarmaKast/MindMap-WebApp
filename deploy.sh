a788a1ae00ca260e61432d11cac7076083aa6b3a
sree chandan <sreechandan98@gmail.com>
2020-04-28 08:11:37 +0530

    github pages deploy setup

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+echo Linting..
+npm run lint
+echo Building. this may take a minute...
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'example.com' > CNAME
+
+echo Deploying..
+git init
+git add -A
+git commit -m 'deploy'
+
+# deploy
+git push -f https://github.com/KarmaKast/MindMap-WebApp.git master:gh-pages
+
+cd -
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/MindMap-WebApp/" : "/",
   configureWebpack: {
     devtool: process.env.NODE_ENV === "development" ? "source-map" : false,
   },
