87b99a305619a5b3ac0e2ef0b1d737793c9be9a4
sree chandan <sreechandan98@gmail.com>
2020-04-29 01:46:48 +0530

    fixes

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+process.env.VUE_APP_VERSION = require("./package.json").version;
+process.env.VUE_APP_NAME = require("./package.json").productName;
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/MindMap-WebApp/" : "/",
   configureWebpack: {
