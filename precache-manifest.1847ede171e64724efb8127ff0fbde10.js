51f51d3ea2bb0347cedf556bc07ea672205f5dc3
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-29 02:51:11 +0000

    Deploy KarmaKast/MindMap-WebApp to github.com/KarmaKast/MindMap-WebApp.git:gh-pages

---
+++ precache-manifest.1847ede171e64724efb8127ff0fbde10.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f19469559996cb2331",
+    "url": "/MindMap-WebApp/css/app.3e2763e4.css"
+  },
+  {
+    "revision": "5bbac221cc6babbb640b1dfe9e874ebb",
+    "url": "/MindMap-WebApp/img/ic_chevron_left_48px.5bbac221.svg"
+  },
+  {
+    "revision": "efcccee41c160a1f9e8c049a4b4ee622",
+    "url": "/MindMap-WebApp/img/ic_more_vert_18px.efcccee4.svg"
+  },
+  {
+    "revision": "009cf001f3435cdad00b2b57fbaa2916",
+    "url": "/MindMap-WebApp/index.html"
+  },
+  {
+    "revision": "90f19469559996cb2331",
+    "url": "/MindMap-WebApp/js/app.37826d00.js"
+  },
+  {
+    "revision": "18642f9de048429f5392",
+    "url": "/MindMap-WebApp/js/chunk-vendors.f29bc709.js"
+  },
+  {
+    "revision": "d87f02aefc5fcddf141eaeb45bec2375",
+    "url": "/MindMap-WebApp/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/MindMap-WebApp/robots.txt"
+  }
+]);
