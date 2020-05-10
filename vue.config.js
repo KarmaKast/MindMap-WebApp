89095b45da35a133e7d7149a76dccdb2ae43fc6b
sree chandan <sreechandan98@gmail.com>
2020-05-10 13:00:32 +0530

    prerender setup fixes

--- vue.config.js
+++ vue.config.js
@@ -6,45 +6,6 @@ const path = require("path");
 const PrerenderSPAPlugin = require("prerender-spa-plugin");
 const Renderer = PrerenderSPAPlugin.PuppeteerRenderer;
 
-const CopyPlugin = require("copy-webpack-plugin");
-
-const productionPlugins = [
-  new PrerenderSPAPlugin({
-    staticDir: path.join(__dirname, "prerender"),
-    indexPath: path.join(
-      __dirname,
-      "prerender",
-      "MindMap-WebApp",
-      "index.html"
-    ),
-    outputDir: path.join(__dirname, "prerender/MindMap-WebApp"),
-    routes: ["/"],
-    //routes: [process.env.NODE_ENV === "production" ? "/" : "/"],
-    renderer: new Renderer({
-      // We need to inject a value so we're able to
-      // detect if the page is currently pre-rendered.
-      injectProperty: "__PRERENDER_INJECTED",
-      inject: {
-        prerendered: true,
-      },
-      //renderAfterElementExists: ".MindMapModule",
-      renderAfterDocumentEvent: "app-rendered",
-      //renderAfterTime: 5000,
-      headless: true,
-    }),
-  }),
-  new CopyPlugin(
-    [
-      {
-        from: path.join(__dirname, "prerender", "MindMap-WebApp"),
-        to: path.join(__dirname, "dist"),
-        toType: "dir",
-      },
-    ],
-    {}
-  ),
-];
-
 console.log(__dirname);
 //console.log(process.env.NODE_ENV);
 //console.log(process.env);
@@ -57,10 +18,36 @@ module.exports = {
     process.env.BUILD_MODE === "prerender"
       ? "./prerender/MindMap-WebApp/"
       : "./dist",
-  //publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   configureWebpack: {
     devtool: process.env.NODE_ENV === "development" ? "source-map" : false,
-    plugins: process.env.BUILD_MODE === "prerender" ? productionPlugins : [],
+  },
+  chainWebpack: (config) => {
+    config.plugin("PrerenderSPAPlugin").use(PrerenderSPAPlugin, [
+      {
+        staticDir: path.join(__dirname, "prerender"),
+        indexPath: path.join(
+          __dirname,
+          "prerender",
+          "MindMap-WebApp",
+          "index.html"
+        ),
+        outputDir: path.join(__dirname, "prerender/MindMap-WebApp"),
+        routes: ["/"],
+        //routes: [process.env.NODE_ENV === "production" ? "/" : "/"],
+        renderer: new Renderer({
+          // We need to inject a value so we're able to
+          // detect if the page is currently pre-rendered.
+          injectProperty: "__PRERENDER_INJECTED",
+          inject: {
+            prerendered: true,
+          },
+          //renderAfterElementExists: ".MindMapModule",
+          renderAfterDocumentEvent: "app-rendered",
+          //renderAfterTime: 5000,
+          headless: true,
+        }),
+      },
+    ]);
   },
   pwa: {
     display: "fullscreen",
